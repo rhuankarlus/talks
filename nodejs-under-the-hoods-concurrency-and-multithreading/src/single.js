51ae340d028fb1abe80e5b46dc10e8a59936c373
rhuankarlusmudano <rhuan.silva@mudano.com>
2020-02-12 16:44:12 -0300

    Removendo código legado

--- nodejs-under-the-hoods-concurrency-and-multithreading/src/single.js
+++ nodejs-under-the-hoods-concurrency-and-multithreading/src/single.js
@@ -55,37 +55,4 @@ app
         console.log(`Process ${process.pid} started`);
     });
 
-// pool.connect().then((client) => {
-//     app
-//     .use((req, res, next) => {
-//         req.initTime = new Date();
-//         req.pg_client = client;
-//         req.pool = pool;
-//         console.log({
-//             url: req.url,
-//             PID: process.pid,
-//         });
-
-//         res.on('finish', () => {
-//             console.log('Finished', {
-//                 url: req.url,
-//                 PID: process.pid,
-//                 time: (Date.now() - req.initTime) / 1000
-//             });
-//         })
-
-//         next();
-//     })
-//     .use(workers)
-//     .use(cpp)
-//     .use(db)
-//     .use(dummy)
-//     .use(fs)
-//     .use(jsProcessing)
-//     .use(request)
-//     .listen(3000, () => {
-//         console.log(`Process ${process.pid} started`);
-//     });
-// });
-
 module.exports = app;
